fb935e2dde2ff1522e3c75d7eb5f7aabf439fd60
Daniel Alzugaray <dalzuga@gmail.com>
2016-01-25 17:31:29 -0800

    features written

--- program.c
+++ program.c
@@ -1,29 +1,8 @@
 program
 {
-
-  /*
-    To compose a feature, write the following:
-
-    featureX;
-
-    where X is a number with one or two digits, less than 21.
-
-    You may write between 2 and 5 features. Each feature is written in a separate line, ending in a semi-colon.
-
-    Example:
-
-    feature1;
-    feature3;
-    feature18;
-
-    Keep in mind, the company you work for has a set policy for conflict resolution.
-
-    When two people push two sets of features, the resulting feature set must contain each feature that was coded, exactly once.
-
-    As a final rule, all comments should be deleted when merging.
-
-   */
-
-  //Feature set goes here. delete me.
-
+  feature2;
+  feature5;
+  feature7;
+  feature13;
+  feature15;
 }
